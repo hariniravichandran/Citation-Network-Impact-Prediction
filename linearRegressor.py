c208ad37791d426dfeb51d7d8650afdb96df72c0
Arun <Arun@Balajis-MacBook-Pro.local>
2017-04-09 12:39:01 -0700

    Updated to include authorCount and referenceCount

--- linearRegressor.py
+++ linearRegressor.py
@@ -17,16 +17,31 @@ def populatePublicationDict(path):
         if line.startswith('#'):
             if line[:2] in mappings or line[:6] in mappings:
                 currLineKey = line[:6] if line[:6] in mappings else line[:2]
-                if mappings[currLineKey] in tempDict and not tempDict[mappings[currLineKey]]:
-                    tempDict[mappings[currLineKey]] = line[6:].replace('\r','').replace('\n','') if line[:6] in mappings else line[2:].replace('\r','').replace('\n','')
+                if currLineKey == '#%':
+                    if not tempDict[mappings[currLineKey]]:
+                        tempDict[mappings[currLineKey]] = []
+                    reference = line[2:].strip().replace('\r','').replace('\n','') if line[2:].strip() else None
+                    if reference:
+                        tempDict[mappings[currLineKey]].append(reference)
+                else:
+                    if mappings[currLineKey] in tempDict and not tempDict[mappings[currLineKey]]:
+                        tempDict[mappings[currLineKey]] = line[6:].replace('\r','').replace('\n','') if line[:6] in mappings else line[2:].replace('\r','').replace('\n','')
         else:
             pubId = tempDict['pubId']
             if pubId not in pubs:
                 pubs[pubId] = {}
                 for k in tempDict:
                     pubs[pubId][k] = tempDict[k]
+                authors = pubs[pubId]['authors']
+                pubs[pubId]['authorCount'] = len(authors.split(',')) if authors else 0
+                references = pubs[pubId]['references']
+                pubs[pubId]['referenceCount'] = len(pubs[pubId]['references'])
             tempDict = {key:None for key in tempDict}
     print pubs
+
+def buildInputFeatures():
+    global pubs
+    
 
 init()
 populatePublicationDict('/Users/agalya/Documents/sml/project/code/Citation-Network-Impact-Prediction/testData.txt')
